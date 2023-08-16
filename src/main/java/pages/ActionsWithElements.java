70f3f26b9428927ace84f42872ec1eb88717845c
iryna_haponenko <irinkagaponenko@gmail.com>
2023-08-16 15:53:37 +0300

    TestCases, Locators, dependencies, resources, libs

---
+++ src/main/java/pages/ActionsWithElements.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ActionsWithElements {
+    Logger logger = Logger.getLogger(getClass());
+    protected WebDriver webDriver;
+    protected WebDriverWait webDriverWait10, webDriverWait15;
+
+    public ActionsWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+        webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
+    }
+}
